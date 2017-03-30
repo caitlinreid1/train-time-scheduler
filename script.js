c793becc046079cfd7ce5ec0a41fe7111d0bdd7c
C. Reid <ctlnrd@gmail.com>
2017-03-30 15:23:22 -0400

    linked external JS file

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
 $('#add-train').on("click", function(event) {
-	alert("hi");
+	event.preventDefault();
+
+	var tNumber = $('#trainNumber-input').val().trim();
+	var dest = $('#destination-input').val().trim()
+	var arr = $('#arrival-input').val().trim()
+	var freq = $('#frequency-input').val().trim()
+
+	console.log(tNumber);
+	console.log(dest);
+	console.log(arr);
+	console.log(freq);
+	
+
+
+
+
 })
+
+$()
